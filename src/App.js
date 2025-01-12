05868daf05e26d4a3db185ace6691d4e63a7e462
Roma Blunt <rdlastochkin@gmail.com>
2025-01-12 19:31:25 +0400

    61

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
-import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
 import News from './components/News/News';
 import Music from './components/Music/Music'
 import Settings from './components/Settings/Settings'
-import Users from './components/Users/Users';
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
 
 const App = (props) => {
   return (
     <Router>
     <div className="app-wrapper">
-      <Header />
+      <HeaderContainer />
       <Nav/>
       <div className='app-wrapper-content'>
            <Routes>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import s from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={s.header}>
         <div >
           <img className={s.img} src='https://static.vecteezy.com/system/resources/thumbnails/024/553/676/small_2x/skull-wearing-crown-logo-skull-king-sticker-pastel-cute-colors-generative-ai-png.png'/>
+        </div>
+        <div className={s.loginBlock}>
+          {props.isAuth ? props.login 
+            : <NavLink to={'/login'}>Login</NavLink> }
+          
         </div>
       </header>
     );
