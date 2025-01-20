c84c682de4e70f7ae625e171325895e3bd3836d9
Roma Blunt <rdlastochkin@gmail.com>
2025-01-20 12:36:44 +0400

    71

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,14 +3,14 @@ import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
 import ProfileInfo from './ProfileInfo/ProfileInfo'
 import MyPostsContainer from './MyPosts/MyPostsContainer';
+import ProfileStatus from './ProfileInfo/ProfileStatus';
 
 const Profile = (props) => {
 
     return(
         <div>
             <ProfileInfo profile={props.profile} />
-            <MyPostsContainer/>
-            
+            <MyPostsContainer />
         </div>
     );
 };
